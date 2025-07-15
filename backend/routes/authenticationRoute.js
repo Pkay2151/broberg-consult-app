fba7aa957968c3c85ef619ee5e6f1a9e313aad95
zonyra-hubert <danielshubert9@gmail.com>
2025-07-15 09:13:05 +0000

    Password reset functionality added

--- backend/routes/authenticationRoute.js
+++ backend/routes/authenticationRoute.js
@@ -1,7 +1,13 @@
 const express = require("express");
-const registerUser = require("../controllers/authenticationController");
+const {
+  registerUser,
+  forgotPassword,
+  resetPassword,
+} = require("../controllers/authenticationController");
 const router = express.Router();
 
-router.post("/register",registerUser)
+router.post("/register", registerUser);
+router.post("/forgot-password", forgotPassword);
+router.put("/reset-password", resetPassword);
 
 module.exports = router;
